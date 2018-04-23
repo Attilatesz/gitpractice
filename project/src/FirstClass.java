c1448b3904bb1a72210d2d38a82d9af16aa0fde7
Attilatesz <attila.sloszar@gmail.com>
2018-04-23 12:25:58 +0200

    b1

--- project/src/FirstClass.java
+++ project/src/FirstClass.java
@@ -1,24 +1,9 @@
 public class FirstClass {
-<<<<<<< HEAD
+    public static void main (String[]args){
+      myAge();
+    }
 
-  public void Greet() {
-    System.out.println("LaszloBrhlik");
-=======
-  public static void main(String[] args) {
-  }
-
-  public void cheer() {
-    System.out.println("A Team");
-
-    myage();
-  }
-
-  private static void myage() {
+  private static void myAge() {
     System.out.println("My age is 28!");
-<<<<<<< HEAD
-=======
->>>>>>> 72e045738186830866229d406ffddb61598166fc
->>>>>>> 9d7844ab65758e5f3eb78dc681557863d4a557df
->>>>>>> 1f0ad171cdb9e4f69c91958bdfba16e5a7d329d5
   }
 }
