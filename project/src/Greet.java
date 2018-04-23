4a6ba6b23eb5773b599abcf7e0ccf3c3781913d8
Laszlo Brhlik <brhlik.laszlo@gmail.com>
2018-04-23 12:29:17 +0200

    a1

---
+++ project/src/Greet.java
@@ -0,0 +1,16 @@
+public class Greet {
+
+  private String aName;
+
+  public Greet(String aName) {
+    System.out.println(aName);
+  }
+
+  public String getaName() {
+    return aName;
+  }
+
+  public void setaName(String aName) {
+    this.aName = aName;
+  }
+}
