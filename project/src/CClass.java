fe2c35b83bd92bfa1eddf464007b39e56f4dfa26
unknown <d.dns0098g@gmail.com>
2018-04-23 12:47:05 +0200

    c5

--- project/src/CClass.java
+++ project/src/CClass.java
@@ -1,8 +1,8 @@
 public class CClass {
   String param = "java";
 
-  public void cheer(String param) {
-    this.param = param;
-    System.out.println(param);
+  public void cheer(String param1, String param2) {
+    param1 = param;
+    System.out.println(param1 + param2);
   }
 }
