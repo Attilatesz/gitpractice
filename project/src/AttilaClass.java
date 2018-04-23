241fb5e54b4978c76a7cc2bd260de369c11e7a7c
Laszlo Brhlik <brhlik.laszlo@gmail.com>
2018-04-23 12:41:55 +0200

    a3

--- project/src/AttilaClass.java
+++ project/src/AttilaClass.java
@@ -1,10 +1,16 @@
 public class AttilaClass {
   public final int age = 28;
+  public final int month = 12;
 
   public int age(){
     return 1 + age;
   }
+
   public void printAge(){
     System.out.println(age);
+  }
+
+  public void printMonth(){
+    System.out.println(month);
   }
 }
