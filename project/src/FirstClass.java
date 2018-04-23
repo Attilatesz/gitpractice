9776ced50a19c578efbfd1e7d7d8b99eb624c29c
Szilard Nemeth <sz.nemeth3@gmail.com>
2018-04-23 12:24:18 +0200

    d1

--- project/src/FirstClass.java
+++ project/src/FirstClass.java
@@ -1,9 +1,7 @@
 public class FirstClass {
-<<<<<<< HEAD
 
   public void Greet() {
     System.out.println("LaszloBrhlik");
-=======
   public static void main(String[] args) {
   }
 
@@ -15,10 +13,6 @@ public class FirstClass {
 
   private static void myage() {
     System.out.println("My age is 28!");
-<<<<<<< HEAD
-=======
->>>>>>> 72e045738186830866229d406ffddb61598166fc
->>>>>>> 9d7844ab65758e5f3eb78dc681557863d4a557df
->>>>>>> 1f0ad171cdb9e4f69c91958bdfba16e5a7d329d5
+
   }
 }
