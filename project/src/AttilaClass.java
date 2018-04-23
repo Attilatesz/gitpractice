4b42f77287a5bd285f4b1e9e454a500160ec21ad
Attilatesz <attila.sloszar@gmail.com>
2018-04-23 12:45:38 +0200

    b4

--- project/src/AttilaClass.java
+++ project/src/AttilaClass.java
@@ -1,10 +1,7 @@
 public class AttilaClass {
-<<<<<<< HEAD
-  public final int age = 28;
+
   public final int month = 12;
-=======
   private final int age = 28;
->>>>>>> cbb9aa17efe0f172ed1b43f37fa8931ad43aa254
 
   public int age(){
     return 1 + age;
